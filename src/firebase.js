3ab7a7b81eb4fc54d1de11f670c7841d3bf18355
Paul  Van Dyke <paulvandyke@Pauls-Air.attlocal.net>
2022-09-08 15:15:41 -0400

    Added env VARS

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaQ2CoIzLi8g1pH7H9-h9-r_dT_Kgfzt0",
-  authDomain: "twitterclone-c0270.firebaseapp.com",
-  projectId: "twitterclone-c0270",
-  storageBucket: "twitterclone-c0270.appspot.com",
-  messagingSenderId: "782638469138",
-  appId: "1:782638469138:web:7e8811441c0598a7d088cb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, 
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSend: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID, 
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -21,4 +21,5 @@ const app = firebase.initializeApp(firebaseConfig);
 
 const db = app.firestore();
 
+//export const auth = db.auth();
 export default db;
